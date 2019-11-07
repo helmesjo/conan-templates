0555b80789de427ccc8a0a631fb09686d4344b67
helmesjo <helmesjo@live.com>
2019-11-07 12:58:43 +0100

    Upped requirement 'boost': 1.67.0 -> 1.71.0

--- conanfile.py
+++ conanfile.py
@@ -26,7 +26,7 @@ class ThriftConan(ConanFile):
 
     # http://thrift.apache.org/docs/install/
     requires = (
-        "boost/1.67.0@conan/stable",
+        "boost/1.71.0@conan/stable",
     )
 
     settings = "os", "arch", "compiler", "build_type"
